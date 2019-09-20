190659d11c5002d387eaf0e59f094dffa3a1403d
Seb <sebastian.cascajares@ephox.com>
2019-09-20 16:09:49 +1000

    Autoexpand_cmd demo with TinyListeners

--- src/com/tinyinswing/demo/Demo.java
+++ src/com/tinyinswing/demo/Demo.java
@@ -4,6 +4,7 @@ import cloud.tiny.tinymceforswing.TinyMCE;
 import cloud.tiny.tinymceforswing.api.config.Config;
 import cloud.tiny.tinymceforswing.api.config.wrappers.ActionApi;
 import cloud.tiny.tinymceforswing.api.events.TinyListener;
+import com.tinyinswing.Utils;
 
 import javax.swing.*;
 import java.awt.*;
@@ -29,7 +30,16 @@ public class Demo extends JFrame {
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setBounds(100, 100, 450, 450);
 
+    /* Creating a TinyListener
+    TinyListeners allows user to execute Java code when the editor calls the window.java.trigger function
+    In this case, we will open a modal window to insert a link in our content
+    TinyListener take two arguments: the name of the trigger and an ActionApi implementation
+     */
     TinyListener insertLink = new TinyListener("javalink", new ActionApi() {
+
+      /*
+      Our ActionApi implementation takes an instance of the editor and a string sent when the trigger was called
+       */
       @Override
       public void run(TinyMCE tinyMCE, String s) {
         final JDialog popup = new JDialog();
@@ -56,6 +66,7 @@ public class Demo extends JFrame {
           @Override
           public void windowClosed(WindowEvent windowEvent) {
             super.windowClosed(windowEvent);
+            // we can interact with the editor from our dialog
             tinyMCE.insertHtml("<a href='http://google.com'>" + textfield.getText() + "</a>");
           }
         });
@@ -66,6 +77,7 @@ public class Demo extends JFrame {
 
     final Config embeddedBased = Config.embedded().setInitConf(this.getClass(), "CustomConfiguration.js").addListener(insertLink);
     final TinyMCE editor = TinyMCE.futureEditor(embeddedBased).get();
+    editor.setBody(Utils.welcomeText);
 
     final JPanel contentPane = new JPanel(new BorderLayout());
     contentPane.add(editor.component(), BorderLayout.CENTER);
