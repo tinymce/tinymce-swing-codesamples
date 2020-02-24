61b8f906128b57866071699023d6f1000c634f45
James Johnson <james.johnson@tiny.cloud>
2020-02-24 13:35:54 +1000

    Minor cleanup

--- src/cloud/tiny/tinyinswing/examples/imagehandling/config.js
+++ src/cloud/tiny/tinyinswing/examples/imagehandling/config.js
@@ -7,10 +7,6 @@
       'insertdatetime media table powerpaste code help wordcount'
     ],
     toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | image | help',
-    content_css: [
-      '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-      '//www.tiny.cloud/css/codepen.min.css'
-    ],
     init_instance_callback: function (editor) {
       console.log('Init instance callback');
       editor.parser.addNodeFilter('img', function (nodes) {
@@ -19,16 +15,13 @@
           var node = nodes[i];
           var src = node.attr('src');
           if (/^file:\/\//.test(src)) {
-            console.log('Matching file://');
+            console.log('Updating ' + src);
             node.attr({
               'src': 'afile' + src.substring(4),
               'data-mce-src': src
             });
           }
         }
-      });
-      editor.on('PostRender', function () {
-        console.log('Post render');
       });
     }
   };
